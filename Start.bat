a1c1f9ec32560139cd295f859931c5b0fd1a19e7
insane <info@insane.software>
2023-07-19 00:59:24 +0200

    V106 Git auto updater + java 8 Desync Fix

--- Start.bat
+++ Start.bat
@@ -1,6 +1,7 @@
 echo off
 color 0A
 cls
-Java\jre8\bin\java.exe -Djava.library.path=lib\native -jar res/updater.jar
-start Java\jre8\bin\java.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
-pause
+cd res/cmd
+git.exe pull
+cd %~dp0
+start Java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
