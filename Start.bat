58a7f9d9853af1c4b0f2834092560db9c1080207
insane <info@insane.software>
2023-07-19 00:43:20 +0200

    Game v105

---
+++ Start.bat
@@ -0,0 +1,6 @@
+echo off
+color 0A
+cls
+Java\jre8\bin\java.exe -Djava.library.path=lib\native -jar res/updater.jar
+start Java\jre8\bin\java.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
+pause
---
+++ res/pokemon/animated/normal/back/034/asd.bat
@@ -0,0 +1,3 @@
+rename 0.png 0.txt
+del *.png
+rename 0.txt 0.png
