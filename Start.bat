a8ebd3f0b02c0bd04973380d83fe4ac1e879f4a1
insane <info@insane.software>
2023-07-19 21:29:37 +0200

    better start

--- Start.bat
+++ Start.bat
@@ -1,8 +1,25 @@
-echo off
+@echo off
+
 color 0A
-cls
-cd updater/cmd
-git.exe pull
-pause
+echo Pokemon Universe Updater
+echo.
+echo Downloading updates...
+echo.
+
+rem Specify the path to the Git executable
+set "gitPath=Updater\cmd\git.exe"
+
+rem Check if the Git executable exists
+if not exist "%gitPath%" (
+    echo Git executable not found. Please provide the correct path.
+    exit /b
+)
+
+rem Change the working directory to the root folder
+cd /d "%~dp0"
+
+rem Perform the Git pull
+"%gitPath%" --work-tree=. pull origin main
+
 cd %~dp0
-start java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
+start Java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
