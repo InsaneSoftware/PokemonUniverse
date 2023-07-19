6f6817cbc7fb3bb46aea6f0e6e4a3ad74fb892a2
insane <info@insane.software>
2023-07-19 22:08:13 +0200

    Old updater

--- Start.bat
+++ Start.bat
@@ -1,25 +1,5 @@
-@echo off
-
+echo off
 color 0A
-echo Pokemon Universe Updater
-echo.
-echo Downloading updates...
-echo.
-
-rem Specify the path to the Git executable
-set "gitPath=Updater\cmd\git.exe"
-
-rem Check if the Git executable exists
-if not exist "%gitPath%" (
-    echo Git executable not found. Please provide the correct path.
-    exit /b
-)
-
-rem Change the working directory to the root folder
-cd /d "%~dp0"
-
-rem Perform the Git pull
-"%gitPath%" --work-tree=. pull origin main
-
-cd %~dp0
+cls
+Java\jre8\bin\java.exe -Djava.library.path=lib\native -jar res/updater.jar
 start Java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
