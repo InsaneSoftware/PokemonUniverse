33a0427047bbc92210875930f919d4aa84e7eb17
insane <info@insane.software>
2023-07-19 21:08:14 +0200

    CLeanup

--- Start.bat
+++ Start.bat
@@ -1,7 +1,8 @@
 echo off
 color 0A
 cls
-cd res/cmd
+cd updater/cmd
 git.exe pull
+pause
 cd %~dp0
-start Java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
+start java\jre8\bin\javaw.exe -Djava.library.path=lib\native -jar PokemonUniverse.jar
